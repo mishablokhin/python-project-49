6a4e17c7fc25038532a9a3a7ad029c7ffdf2be1f
Михаил Блохин <mishablokhin@yandex.ru>
2024-01-18 16:55:16 +0300

    Moved all  greeting code to cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,8 +3,8 @@ from brain_games.cli import welcome_user
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    print(welcome_user())
+    greeting, _ = welcome_user()
+    print(greeting)
 
 
 if __name__ == '__main__':
