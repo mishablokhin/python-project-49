71fc65acb88829ed89a5a38fdc612d957a9cf5e5
Михаил Блохин <mishablokhin@yandex.ru>
2024-02-01 13:24:07 +0300

    Changed function output to str

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,4 +4,4 @@ from random import randint
 def even_or_not():
     question = randint(1, 100)
     correct_answer = 'yes' if question % 2 == 0 else 'no'
-    return question, correct_answer
+    return str(question), correct_answer
