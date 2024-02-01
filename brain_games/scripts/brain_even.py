39b811ec3088f124417c21e57face4d2d41fd805
Михаил Блохин <mishablokhin@yandex.ru>
2024-02-01 11:35:07 +0300

    Fixed linter errors

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,10 +3,12 @@ from brain_games.cli import welcome_user
 from brain_games.game_control import run_game
 from brain_games.games.even import even_or_not
 
+
 def main():
     greeting, name = welcome_user()
     print(greeting)
     run_game(even_or_not)
+
 
 if __name__ == '__main__':
     main()
