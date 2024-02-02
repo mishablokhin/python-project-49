5e587a2581e313dab72384b4269d560896254759
Михаил Блохин <mishablokhin@yandex.ru>
2024-02-02 12:25:15 +0300

    Added script to run brain-progression game

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome_user
+from brain_games.game_control import run_game
+from brain_games.games.progression import progression
+
+
+def main():
+    greeting, name = welcome_user()
+    print(greeting)
+    print('What number is missing in the progression?')
+    run_game(progression)
+
+
+if __name__ == '__main__':
+    main()
