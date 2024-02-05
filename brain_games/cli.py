aa22392800471d51323260b81d8e7ce9c8f3d2c6
Михаил Блохин <mishablokhin@yandex.ru>
2024-02-05 20:12:00 +0300

    Reverted cli.py to suits 3 task with no name return from function

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,5 +4,4 @@ import prompt
 def welcome_user():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    greeting = f'Hello, {name}!'
-    return greeting, name
+    print(f"Hello, {name}!")
