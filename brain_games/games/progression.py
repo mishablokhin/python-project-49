afe9c41af502f004a9bf218141d632c82c8b739e
Михаил Блохин <mishablokhin@yandex.ru>
2024-02-05 20:21:06 +0300

    Rewrite file with using range function

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,17 +2,21 @@ from random import randint
 
 
 def progression():
-    question = ''
-    progression_list = []
     progression_start = randint(1, 20)
     progression_difference = randint(1, 10)
-    progression_lenght = randint(5, 10)
-    for i in range(progression_lenght):
-        progression_list.append(progression_start + progression_difference * i)
-    hidden_digit_index = randint(0, len(progression_list) - 1)
+    progression_length = randint(5, 10)
+    progression_list = list(
+        range(
+            progression_start,
+            progression_start + progression_difference * progression_length,
+            progression_difference
+        )
+    )
+    hidden_digit_index = randint(0, progression_length - 1)
     hidden_digit = progression_list[hidden_digit_index]
     progression_list[hidden_digit_index] = '..'
-    for i in range(0, len(progression_list)):
-        question += str(progression_list[i]) + ' '
+
+    question = ' '.join(map(str, progression_list))
     correct_answer = str(hidden_digit)
+
     return question, correct_answer
