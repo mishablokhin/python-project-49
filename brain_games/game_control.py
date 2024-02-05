d7b93d84874823ca740280fd03088226dfc0ec5f
Михаил Блохин <mishablokhin@yandex.ru>
2024-02-05 20:12:54 +0300

    Changed logic to use for/else

--- brain_games/game_control.py
+++ brain_games/game_control.py
@@ -1,17 +1,15 @@
 def run_game(game_name, user_name):
-    correct_answers = 0
-    while correct_answers < 3:
+    for _ in range(3):
         question, correct_answer = game_name()
         print('Question:', question)
         user_answer = input('Your answer: ')
 
         if user_answer == correct_answer:
             print('Correct!')
-            correct_answers += 1
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
                   f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {user_name}!")
-            return
-
-    print(f"Congratulations, {user_name}!")
+            break
+    else:
+        print(f"Congratulations, {user_name}!")
