ac48fd24ffc709940320c42ae6f3bcb69944b0e4
Михаил Блохин <mishablokhin@yandex.ru>
2024-02-05 20:13:48 +0300

    Rewrite file  without eval function

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,11 +2,20 @@ import random
 from random import randint
 
 
+def calculate(a, b, operation):
+    if operation == '+':
+        return a + b
+    elif operation == '-':
+        return a - b
+    elif operation == '*':
+        return a * b
+
+
 def calculator():
-    operations_list = ['+', '-', '*']
+    operations = ['+', '-', '*']
     a = randint(1, 25)
     b = randint(1, 25)
-    operation = random.choice(operations_list)
+    operation = random.choice(operations)
     question = f"{a} {operation} {b}"
-    correct_answer = str(eval(question))
+    correct_answer = str(calculate(a, b, operation))
     return question, correct_answer
